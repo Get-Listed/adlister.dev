16710838f8a7aaae4b3980d75e8803331c2ba8be
J.D. Garza <jdgarza22@yahoo.com>
2015-04-23 12:10:52 -0500

    deleted unnecessary text

--- database/get_listed_seeder.php
+++ database/get_listed_seeder.php
@@ -1,14 +1,7 @@
 <?php
-
-<<<<<<< HEAD
 define('DB_USER', 'parks_user');
 define('DB_NAME', 'get_listed_db');
 define('DB_PASS', 'tokyo3');
-=======
-define('DB_USER', 'codeup');
-define('DB_NAME', 'get_listed_db');
-define('DB_PASS', 'password');
->>>>>>> 4e59dcc0972b95044c20055e2d9ebd8b8beff524
 define('DB_HOST', '127.0.0.1');
 
 require '../db_connect.php';
