335b5f1781847b1bf25dd72248fbab5126209dce
J.D. Garza <jdgarza22@yahoo.com>
2015-04-23 13:06:56 -0500

    deleted unnecessary text

--- db_connect.php
+++ db_connect.php
@@ -1,15 +1,4 @@
 <?php
-<<<<<<< HEAD
-define('DB_USER', 'parks_user');
-define('DB_NAME', 'get_listed_db');
-define('DB_PASS', 'tokyo3');
-define('DB_HOST', '127.0.0.1');
 
 $dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-=======
-
-$dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
-$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
->>>>>>> 4e59dcc0972b95044c20055e2d9ebd8b8beff524
